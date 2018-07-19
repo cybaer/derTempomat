0ff12ddff3be00e1609a16bcafeedee0b71e3bda
cybaer <s.k.g@gmx.de>
2018-07-19 15:31:44 +0200

    first commit

--- HardwareConfig.h
+++ HardwareConfig.h
@@ -1,7 +1,7 @@
 /*
  * HardwareConfig.h
  *
- *  Created on: 09.02.2017
+ *  Created on: 19.07.2018
  *      Author: cybaer
  *
  * This program is free software: you can redistribute it and/or modify
@@ -21,7 +21,6 @@
 #define HARDWARECONFIG_H_
 
 #include "lib/ui/EdgeTrigger.h"
-#include "lib/ui/analogSwitch.h"
 #include "avrlib/base.h"
 #include "avrlib/gpio.h"
 #include "avrlib/adc.h"
@@ -29,75 +28,25 @@
 using namespace avrlib;
 static const uint8_t MAX_CV = 122;
 
-template <typename Out_1, typename Out_2, typename Out_3, typename Out_4>
-class SequentialSwitch
-{
-public:
-  void activateNextStep(void)
-  {
-    if(++m_Step > m_StepCount)
-      m_Step = 0;
-    inactivateAll();
-    m_ActivatFunctions[m_Step]();
-  }
-  void setStepCount(uint8_t count)
-  {
-    if(count != m_StepCount)
-    {
-      m_StepCount = count > 3 ? 3 : count;
-      if(count == 0)
-      {
-        onReset();
-      }
-    }
-  }
-  void onReset(void)
-  {
-    m_Step = 0;
-    inactivateAll();
-    if(m_StepCount != 0)
-      m_ActivatFunctions[m_Step]();
-  }
-
-private:
-  static void inactivateAll(void)
-  {
-    Out_1::Low();
-    Out_2::Low();
-    Out_3::Low();
-    Out_4::Low();
-  }
-
-  typedef void (*FP)(void);
-
-  const FP m_ActivatFunctions[4] = {Out_1::High, Out_2::High, Out_3::High, Out_4::High};
-  uint8_t m_StepCount;
-  uint8_t m_Step;
-};
-
-typedef EdgeTrigger<Gpio<PortD, 7>, 0> Trigger;  // 13
+typedef EdgeTrigger<Gpio<PortD, 7>, 0> Clock;  // 13
 typedef EdgeTrigger<Gpio<PortB, 2>, 0> ResetIn;  // 16
 
 typedef Inverter<Gpio<PortD, 4> > Output_1;  // 6
 typedef Inverter<Gpio<PortD, 2> > Output_2;  // 4
 typedef Inverter<Gpio<PortD, 1> > Output_3;  // 3
 typedef Inverter<Gpio<PortD, 0> > Output_4;  // 2
+typedef Inverter<Gpio<PortD, 3> > Output_5;  // x
 
-SequentialSwitch<Output_1, Output_2, Output_3, Output_4> SeqSwitch;
-
-typedef Inverter<Gpio<PortB, 6> > OutputReturn;  // 9
 typedef Gpio<PortB, 7> Debug;                    // 10
 
 static const uint8_t AdcChannelCV = 1;
-static const uint8_t AdcChannelSwitch = 4;
-typedef AnalogSwitch<Adc, 5, AdcChannelSwitch> SwitchSteps;
 
 extern Adc adc;
 
 inline void initInputs(void)
 {
   ResetIn::init();
-  Trigger::init();
+  Clock::init();
 }
 inline void initOutputs(void)
 {
@@ -109,8 +58,8 @@ inline void initOutputs(void)
   Output_3::set_value(false);
   Output_4::set_mode(DIGITAL_OUTPUT);
   Output_4::set_value(false);
-  OutputReturn::set_mode(DIGITAL_OUTPUT);
-  OutputReturn::set_value(false);
+  Output_5::set_mode(DIGITAL_OUTPUT);
+  Output_5::set_value(false);
   Debug::set_mode(DIGITAL_OUTPUT);
   Debug::set_value(true);
 }
@@ -126,6 +75,5 @@ inline void initHW(void)
   initOutputs();
   initAnalogIn();
 }
-
 
 #endif /* HARDWARECONFIG_H_ */
--- derTempomat.cpp
+++ derTempomat.cpp
@@ -10,71 +10,30 @@
 #include "avrlib/gpio.h"
 #include "avrlib/adc.h"
 #include "HardwareConfig.h"
-#include "BernoulliGate.h"
-
-BernoulliGate<Output_1, Output_2> bernoulliGate;
 
 int main(void)
 {
   sei();
   initHW();
-  Adc::StartConversion(AdcChannelSwitch);
-  bernoulliGate.setMaxValue(MAX_CV);
-  bernoulliGate.setThreshold(64);
+  Adc::StartConversion(AdcChannelCV);
 
   while(1)
   {
-    static uint8_t adcChannel = AdcChannelSwitch;
-    static const uint8_t adcChannels[] = {AdcChannelSwitch, AdcChannelCV};
     static uint8_t step = 3;
 
     if (Adc::ready())
     {
-      switch(adcChannel)
-      {
-      case AdcChannelSwitch:
-      {
-        step = SwitchSteps::getValue();
-        Adc::StartConversion(AdcChannelCV);
-        SeqSwitch.setStepCount(step);
-        adcChannel = AdcChannelCV;
-        break;
-      }
-      case AdcChannelCV:
-      {
         uint8_t threshold = MAX_CV - (Adc::Read(AdcChannelCV) >> 2) & 0xFF;
-        Adc::StartConversion(AdcChannelSwitch);
-        bernoulliGate.setThreshold(threshold);
-        adcChannel = AdcChannelSwitch;
-        break;
-      }
-      default: adcChannel = 0;
-      }
+        Adc::StartConversion(AdcChannelCV);
+
     }
 
     if(ResetIn::isTriggered())
     {
-      //BernoulliGate.onReset();
-      SeqSwitch.onReset();
+
     }
 
-    if(step == 0)
-    {
-      const bool triggerVal = Trigger::getValue();
-      if(Trigger::isTriggered())
-      {
-        bernoulliGate.activateNextStep();
-        // prevents glitches through hardware latencies
-        _delay_us(200.0);
-      }
-      OutputReturn::set_value(triggerVal);
-    }
-    else
-    {
-      if(Trigger::isTriggered())
-      {
-        SeqSwitch.activateNextStep();
-      }
-    }
+    Clock::isTriggered();
+
   }
 }
